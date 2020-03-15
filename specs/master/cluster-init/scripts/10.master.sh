a42cc4a63d2c0c40b2cfe608db0c8d897890e016
hirtanak <hirtanak@gmail.com>
2020-03-15 11:55:22 +0900

    add to use custom hostname prompt

--- specs/master/cluster-init/scripts/10.master.sh
+++ specs/master/cluster-init/scripts/10.master.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# Copyright (c) 2019 Hiroshi Tanaka, hirtanak@gmail.com @hirtanak
+# Copyright (c) 2020 Hiroshi Tanaka, hirtanak@gmail.com @hirtanak
 set -exuv
 
 echo "starting 10.master.sh"
@@ -17,13 +16,13 @@ CUSER=$(grep "Added user" /opt/cycle/jetpack/logs/jetpackd.log | awk '{print $6}
 CUSER=${CUSER//\'/}
 CUSER=${CUSER//\`/}
 # After CycleCloud 7.9 and later 
-if [[ -z $CUSER ]]; then 
+if [[ -z $CUSER ]]; then
    CUSER=$(grep "Added user" /opt/cycle/jetpack/logs/initialize.log | awk '{print $6}' | head -1)
    CUSER=${CUSER//\`/}
 fi
 echo ${CUSER} > /mnt/exports/shared/CUSER
 HOMEDIR=/shared/home/${CUSER}
-CYCLECLOUD_SPEC_PATH=/mnt/cluster-init/QuntumESPRESSO/master
+CYCLECLOUD_SPEC_PATH=/mnt/cluster-init/QCMD/master
 
 # get Quantum ESPRESSO version
 QE_VERSION=$(jetpack config QE_VERSION)
@@ -44,9 +43,20 @@ chown -R ${CUSER}:${CUSER} /mnt/exports/apps | exit 0
 
 yum install -y htop
 
+set +u
+CMD=$(grep PS1 ${HOMEDIR}/.bashrc) | exit 0
+#CMD1=$(grep "Getting latest auth configuration" /opt/cycle/jetpack/logs/initialize.log | head -1 | awk '{print $9}') | exit 0
+(grep "Getting latest auth configuration" /opt/cycle/jetpack/logs/initialize.log | head -1 | awk '{print $9}') > ${HOMEDIR}/hostname
+CMD2=$(cat ${HOMEDIR}/hostname) && (echo "${CMD2##*\?cluster\=}") > ${HOMEDIR}/hostname
+CMD2=$(cat ${HOMEDIR}/hostname)
+if [[ -z ${CMD}  ]]; then
+   (echo "export PS1='[\\u@${CMD2} \\h \\W]'") >> ${HOMEDIR}/.bashrc
+fi
+set -u
+
 # file settings
-chown -R ${CUSER}:${CUSER} ${HOMEDIR}/apps 
-cp /opt/cycle/jetpack/logs/cluster-init/QuantumESPRESSO/master/scripts/10.master.sh.out ${HOMEDIR}/ 
+chown -R ${CUSER}:${CUSER} ${HOMEDIR}/apps
+cp /opt/cycle/jetpack/logs/cluster-init/QuantumESPRESSO/master/scripts/10.master.sh.out ${HOMEDIR}/
 chown ${CUSER}:${CUSER} ${HOMEDIR}/10.master.sh.out
 
 #clean up
